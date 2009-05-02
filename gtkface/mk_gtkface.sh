5c22c1ba7e28264a46905d9ddc6d80005efb555f
lexs <lexs@lexszero-msi.(none)>
2009-05-03 00:22:13 +0400

    Makefile for gHateXMPP

--- gtkface/mk_gtkface.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# mk_gtkface.sh
-
-gcc -c gtkface.c -Os -g `pkg-config --cflags --libs gtk+-2.0` -o libgtkface.a
-gcc gmain.c -L. -lgtkface -Os -g `pkg-config --cflags --libs gtk+-2.0` -o gmain 
-
