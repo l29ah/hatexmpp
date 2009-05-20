07bc16a43fc6a8c1b8660934fc975f4781b3f652
Sergey Alirzaev <zl29ah@gmail.com>
2009-05-20 17:46:22 +0400

    more hate to screen

--- hatescreen.sh
+++ hatescreen.sh
@@ -2,11 +2,11 @@
 tmprc="/tmp/.hatescreen"
 
 cat > $tmprc << EOF
-screen tail -f $1
+screen bash -c 'tail -f $1 | (while read d m; do echo -E \$(date +%H:%M:%S -d @\$d) "\$m"; done)'
 split
 focus
 resize 5
-screen bash -c 'while :; do read s; echo -n \$s > '$1'; done'
+screen bash -c 'while read s; do echo -nE "\$s" >> '$1'; done'
 EOF
 
 screen -t "$1" -c "$tmprc"
