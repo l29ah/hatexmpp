6b6afd3a14c341524d224359a52d672a999290c6
lexszero <lexszero@gmail.com>
2009-05-20 18:03:31 +0400

    [hatescreen] +quit binding, +rlwrap

--- hatescreen.sh
+++ hatescreen.sh
@@ -2,11 +2,12 @@
 tmprc="/tmp/.hatescreen"
 
 cat > $tmprc << EOF
+bind Q quit
 screen bash -c 'tail -f $1 | (while read d m; do echo -E \$(date +%H:%M:%S -d @\$d) "\$m"; done)'
 split
 focus
 resize 5
-screen bash -c 'while read s; do echo -nE "\$s" >> '$1'; done'
+screen rlwrap bash -c 'while read s; do clear; echo -nE "\$s" >> '$1'; done'
 EOF
 
 screen -t "$1" -c "$tmprc"
