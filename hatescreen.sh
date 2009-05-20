5a75aba6de26e351a6dc7d014112aa7de24ba71d
x3al <x3al@3arch.localdomain>
2009-05-20 20:23:21 +0400

    semi-fixed: treating \ as escape chars; fixed: now input treats as a line, should work better with console input.

--- hatescreen.sh
+++ hatescreen.sh
@@ -4,11 +4,11 @@ f=`basename $1`
 cd `dirname $1`
 cat > $tmprc << EOF
 bind Q quit
-screen bash -c 'tail -f $f | (while read d n m; do if dd=\$(date +%H:%M:%S -d @\$d); then echo -En "\$dd"; else echo -En "\$d"; fi; echo -en "\E[3\$((\$(echo \$n | sum | cut -c1) % 7 +1));40m"; echo -ne " \$n\E[37;40m"; echo -E " \$m";  done)'
+screen bash -c 'tail -f $f | (while read -r d n m; do if dd=\$(date +%H:%M:%S -d @\$d); then echo -En "\$dd"; else echo -En "\$d"; fi; echo -en "\E[3\$((\$(echo \$n | sum | cut -c1) % 7 +1));40m"; echo -ne " \$n\E[37;40m"; echo -E " \$m";  done)'
 split
 focus
 resize 5
-screen rlwrap -c bash -c 'while read s; do clear; echo -nE "\$s" >> '$f'; done'
+screen rlwrap -c bash -c 'while read -e s; do clear; echo -nE "\$s" >> '$f'; done'
 EOF
-
+ 
 screen -t "$f" -c "$tmprc"
