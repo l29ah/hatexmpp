f9e70ee74e253196a510624edd9b32dfe61a343c
Sergey Alirzaev <zl29ah@gmail.com>
2009-05-23 03:11:21 +0400

    + message cutter to hatebot

--- frontends/hatebot/cmdparse.sh
+++ frontends/hatebot/cmdparse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CMD=$1
 shift
-commands/$CMD $* 2> /dev/null
+commands/$CMD $* 2> /dev/null | perl -e 'use utf8; $i=0; $max=500; while (<STDIN>) {if (($i+=100+length($_)) < $max) { print "$_"; } else { print substr("$_", 0, ($i-$max)); print "\n"; exit; }}'
 
